d7eac88d108736bef4ab6d0e420a5795601e762d
Francisco Barillas <61847258+fbarillas08@users.noreply.github.com>
2021-03-18 14:21:09 -0400

    Updating sql, jupyter, output, and resource files
    Final upload of all code and input/output files

--- License_expiration.sql
+++ License_expiration.sql
@@ -1,7 +1,8 @@
 --- Nuclear Reactor License Expiration
-
 select
-	B.reactor_type as "Reactor Type",
-	count(B.reactor_type) as "# Reactors"
+	B.docket_number as "Docket Number",
+	B.unit_name as "Unit Name",
+	B.operating_mw as "Rated MW",
+	B.license_exp as "License Expiration Date"
 from orboilerplate as B
-	group by B.reactor_type
+	order by B.license_exp
